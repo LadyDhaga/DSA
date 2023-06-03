722146ee8bf72cc897a3227d03d0c772877a424f
LadyDhaga <navdeepjaj016@gmail.com>
2023-06-03 13:54:53 +0530

    new commmit

--- BinaryTreeUse.cpp
+++ BinaryTreeUse.cpp
@@ -1,7 +1,9 @@
+#include "BinarySearchTree.h"
 #include <iostream>
 #include <queue>
-#include "BinaryTreeNode.h"
+// #include "BinaryTreeNode.h"
 #include <bits/stdc++.h>
+#include "ListNode.cpp"
 #include <unordered_map>
 using namespace std;
 
@@ -323,6 +325,238 @@ void inRange(BinaryTreeNode<int> *root, vector<int> &v, int r1, int r2)
     inRange(root->sajja, v, r1, r2);
     return;
 }
+
+int maximum(BinaryTreeNode<int> *root)
+{
+    if (!root)
+        return INT_MIN;
+    return max(root->data, max(maximum(root->khabba), maximum(root->sajja)));
+}
+int minimum(BinaryTreeNode<int> *root)
+{
+    if (!root)
+        return INT_MAX;
+    return min(root->data, min(minimum(root->khabba), minimum(root->sajja)));
+}
+
+bool validBST(BinaryTreeNode<int> *root)
+{
+    if (!root)
+        return true;
+    auto l = maximum(root->khabba);
+    auto m = minimum(root->sajja);
+    bool output = (root->data > l) && (root->data <= m) && (validBST(root->khabba)) && (validBST(root->sajja));
+    return output;
+}
+pair<bool, pair<int, int>> isBST(BinaryTreeNode<int> *root)
+{
+    if (!root)
+    {
+        auto p = make_pair(true, make_pair(INT_MIN, INT_MAX));
+        return p;
+    }
+    auto p = isBST(root->khabba);
+    auto q = isBST(root->sajja);
+    bool n = p.first && q.first && (root->data > p.second.first) && (root->data <= q.second.second);
+    auto m = make_pair(n, make_pair(max(root->data, max(p.second.first, q.second.first)), min(root->data, min(p.second.second, q.second.second))));
+    return m;
+}
+
+BinaryTreeNode<int> *constructBstFromSortedArray(vector<int> &v, int m, int n)
+{
+    if (m > n)
+        return NULL;
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(v[(m + n) / 2]);
+    root->khabba = constructBstFromSortedArray(v, m, ((m + n) / 2) - 1);
+    root->sajja = constructBstFromSortedArray(v, (m + n) / 2 + 1, n);
+    return root;
+}
+
+void levelLinkedListHelper(queue<BinaryTreeNode<int> *> &bus, vector<ListNode<int> *> &v)
+{
+    if (bus.empty())
+        return;
+    auto front = bus.front();
+    bus.pop();
+    ListNode<int> *head = new ListNode<int>(front->data);
+    int n = bus.size();
+    while (n--)
+    {
+        auto newfront = bus.front();
+        bus.pop();
+        head->next = new ListNode<int>(newfront->data);
+        if (newfront->khabba)
+            bus.push(newfront->khabba);
+        if (newfront->sajja)
+            bus.push(newfront->sajja);
+    }
+    v.push_back(head);
+    levelLinkedListHelper(bus, v);
+}
+vector<ListNode<int> *> levelLinkedList(BinaryTreeNode<int> *root)
+{
+    if (!root)
+        return {};
+    queue<BinaryTreeNode<int> *> bus;
+    bus.push(root);
+    vector<ListNode<int> *> v;
+    levelLinkedListHelper(bus, v);
+    return v;
+}
+
+void noSiblings(BinaryTreeNode<int> *root, vector<int> &v)
+{
+    if (!root)
+        return;
+    if (root->khabba && root->sajja)
+    {
+        noSiblings(root->khabba, v);
+        noSiblings(root->sajja, v);
+        return;
+    }
+    if (root->khabba)
+    {
+        v.emplace_back(root->khabba->data);
+        noSiblings(root->khabba, v);
+    }
+    if (root->sajja)
+    {
+        v.emplace_back(root->sajja->data);
+        noSiblings(root->sajja, v);
+    }
+    return;
+}
+
+void findPathHelper(BinaryTreeNode<int> *root, int k, vector<int> &v)
+{
+    if (!root)
+        return;
+    if (root->data < k)
+    {
+        findPathHelper(root->sajja, k, v);
+        v.emplace_back(root->data);
+        return;
+    }
+    if (root->data > k)
+    {
+        findPathHelper(root->khabba, k, v);
+        v.emplace_back(root->data);
+        return;
+    }
+    v.emplace_back(k);
+    return;
+}
+
+vector<int> findPath(BinaryTreeNode<int> *root, int k)
+{
+    if (!root)
+        return {};
+    vector<int> v;
+    findPathHelper(root, k, v);
+    return v;
+}
+
+void insertDuplicates(BinaryTreeNode<int> *node)
+{
+    if (!node)
+        return;
+    auto newNode = node->khabba;
+    auto anotherNode = new BinaryTreeNode<int>(node->data);
+    node->khabba = anotherNode;
+    anotherNode->khabba = newNode;
+    insertDuplicates(newNode);
+    insertDuplicates(node->sajja);
+    return;
+}
+
+void pairSumBinaryTreeHelper(BinaryTreeNode<int> *root, unordered_set<int> &umap, int &s)
+{
+    if (!root)
+        return;
+    if (umap.find(s - root->data) != umap.end())
+    {
+        if (s - root->data >= root->data)
+        {
+            cout << root->data << " " << s - root->data << endl;
+        }
+        else
+            cout << s - root->data << " " << root->data << endl;
+    }
+    umap.insert(root->data);
+    pairSumBinaryTreeHelper(root->khabba, umap, s);
+    pairSumBinaryTreeHelper(root->sajja, umap, s);
+    return;
+}
+
+BinaryTreeNode<int> *finder(BinaryTreeNode<int> *root, int k)
+{
+    if (!root)
+        return NULL;
+    if (root->data == k)
+        return root;
+    auto p = finder(root->khabba, k);
+    auto q = finder(root->sajja, k);
+    return p ? p : q;
+}
+
+bool find(BinaryTreeNode<int> *root, int k)
+{
+    if (!root)
+        return false;
+    if (root->data == k)
+        return true;
+    return find(root->khabba, k) || find(root->sajja, k);
+}
+
+void printPairs(BinaryTreeNode<int> *cRoot, int k, BinaryTreeNode<int> *root)
+{
+    if (!cRoot)
+        return;
+    printPairs(cRoot->sajja, k, root);
+    printPairs(cRoot->khabba, k, root);
+    if (cRoot->data != INT_MIN)
+    {
+        auto newNode = finder(root, k - cRoot->data);
+        if (newNode)
+        {
+            if (cRoot->data < k - cRoot->data)
+            {
+                cout << cRoot->data << " " << k - cRoot->data << endl;
+            }
+            else
+                cout << k - cRoot->data << " " << cRoot->data << endl;
+            newNode->data = INT_MIN;
+            return;
+        }
+        return;
+    }
+    return;
+}
+
+void pairSumBinaryTree_withHashmap(BinaryTreeNode<int> *root, int &s)
+{
+    if (!root)
+        return;
+    unordered_set<int> umap;
+    umap.insert(root->data);
+    pairSumBinaryTreeHelper(root, umap, s);
+    return;
+}
+
+BinaryTreeNode<int> *LCA(BinaryTreeNode<int> *root, int k, int l)
+{
+    if (!root)
+        return NULL;
+    if (find(root->khabba, k) && find(root->khabba, l))
+    {
+        return LCA(root->khabba, k, l);
+    }
+    if (find(root->sajja, k) && find(root->sajja, l))
+    {
+        return LCA(root->sajja, k, l);
+    }
+}
+
 int main()
 {
     // BinaryTreeNode<int> *jadd = new BinaryTreeNode<int>(1);
@@ -363,12 +597,33 @@ int main()
     // cin >> y;
     // vector<int> v;
     // inRange(jadd, v, x, y);
-    // for (auto i : v)
-    // {
-    //     cout << i << endl;
-    // }
     // 1 2 3 -1 -1 -1 -1
     // 8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
+    // cout << validBST(jadd) << endl;
+    // cout << validBST(jadd) << endl;
+    // int n;
+    // cin >> n;
+    // vector<int> v(n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> v[i];
+    // }
+    // BinaryTreeNode<int> *jadd = constructBstFromSortedArray(v, 0, n - 1);
+    // noSiblings(jadd, v);
+    // cout << endl;
+    // int k;
+    // cin >> k;
+    // vector<int> v = findPath(jadd, k);
+    // for (auto i : v)
+    // {
+    //     cout << i << " ";
+    // }
+    // printTreeLevel(jadd);
+    // insertDuplicates(jadd);
     BinaryTreeNode<int> *jadd = takeInputLevel();
-    delete jadd;
+    int k;
+    cin >> k;
+    cout << endl;
+    printPairs(jadd, k, jadd);
+    // printTree(jadd);
 }
